cfbfb0673d421f76f8b26b3fd867642df5bb0883
MohammadNidal99 <mnmakhadmeh99@gmail.com>
2021-08-15 12:36:07 +0300

    Task 6

---
+++ src/main/java/jo/secondstep/hrServises/MySqlConnection1Application.java
@@ -0,0 +1,14 @@
+package jo.secondstep.hrServises;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySqlConnection1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySqlConnection1Application.class, args);
+	}
+
+}
